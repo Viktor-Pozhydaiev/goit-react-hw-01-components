2d2597cf98011365a5fcc4a15e574671c7f58090
Viktor <jude.peper@gmail.com>
2022-11-22 21:22:12 +0100

    firs task succses

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { Profile } from './profile/Profile';
+import user from '../user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile user={user} />
+    </>
   );
 };
